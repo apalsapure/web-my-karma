2bf3bbbd1fa3f520cce0c024d313550dc90210e7
Amar Palsapure <amar.palsapure@gmail.com>
2015-05-09 01:52:30 +0530

    initial commit

---
+++ KarmaRewards.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace KarmaRewards.Web
+{
+    public class BundleConfig
+    {
+        public class NonOrderingBundleOrderer : IBundleOrderer
+        {
+            public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+            {
+                return files;
+            }
+        }
+
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/resources/css/primary")
+                .Include(
+                    "~/Resources/plugins/font-awesome/css/font-awesome.min.css",
+                    "~/Resources/plugins/jquery-ui/jquery-ui-1.10.3.custom.min.css",
+                    "~/Resources/plugins/bootstrap/css/bootstrap.min.css",
+                    "~/Resources/plugins/uniform/css/uniform.default.css",
+                    "~/Resources/plugins/bootstrap-switch/css/bootstrap-switch.min.css",
+                    "~/Resources/plugins/simple-line-icons/simple-line-icons.css",
+
+                    //"~/Resources/css/components.css",
+                    "~/Resources/css/plugins.css",
+                    "~/Resources/css/structure.css",
+                    "~/Resources/css/base.css",
+                    "~/Resources/css/custom.css"
+                    ));
+
+            var primaryBundle = new ScriptBundle("~/bundles/scripts/primary")
+                 .Include(
+                    "~/Resources/plugins/jquery.min.js",
+                    "~/Resources/plugins/jquery-migrate.min.js",
+                    "~/Resources/plugins/jquery-ui/jquery-ui-1.10.3.custom.min.js",
+                    "~/Resources/plugins/bootstrap/js/bootstrap.js",
+                    "~/Resources/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js",
+                    "~/Resources/plugins/jquery-slimscroll/jquery.slimscroll.min.js",
+                    "~/Resources/plugins/jquery.blockui.min.js",
+                    "~/Resources/plugins/uniform/jquery.uniform.min.js",
+                    "~/Resources/plugins/bootstrap-switch/js/bootstrap-switch.min.js",
+                    "~/Resources/plugins/jquery-validation/js/jquery.validate.js",
+                    "~/Resources/plugins/jquery-validation/js/additional-methods.js",
+                    "~/Resources/plugins/app.js"
+                    );
+
+            primaryBundle.Orderer = new NonOrderingBundleOrderer();
+
+            bundles.Add(primaryBundle);
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts/login")
+                .Include(
+                    "~/Resources/plugins/backstretch/jquery.backstretch.min.js"
+                ));
+        }
+    }
+}
---
+++ KarmaRewards.Web/Controllers/ErrorController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace KarmaRewards.Web.Controllers
+{
+    public class ErrorController : BaseController
+    {
+        protected override void HandleUnknownAction(string actionName)
+        {
+            if (this.GetType() != typeof(ErrorController))
+            {
+                var errorRoute = new RouteData();
+                errorRoute.Values.Add("controller", "Error");
+                errorRoute.Values.Add("action", "Http404");
+                errorRoute.Values.Add("url", HttpContext.Request.Url.OriginalString);
+
+                View("Http404").ExecuteResult(this.ControllerContext);
+            }
+        }
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            //TODO: Need to handle session expired
+            if (filterContext.Exception != null
+                && !string.IsNullOrWhiteSpace(filterContext.Exception.Message)
+                && filterContext.Exception.Message.StartsWith("Invalid user token provided"))
+            {
+                Response.Redirect("/account/logout");
+            }
+
+            base.OnException(filterContext);
+        }
+
+        public ActionResult Main()
+        {
+            return View("Error");
+        }
+
+        public ActionResult Http404()
+        {
+            return View("Http404");
+        }
+
+        public ActionResult Http403()
+        {
+            return View("Http403");
+        }
+
+        public ActionResult AccessDenied()
+        {
+            return Http403();
+        }
+    }
+}
